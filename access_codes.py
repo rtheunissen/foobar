ed5510d6f0a605a707037bade1fa2d5512ad07c6
Rudi Theunissen <rudolf.theunissen@gmail.com>
2014-12-03 22:47:08 +1300

    Added access_codes

---
+++ access_codes.py
@@ -0,0 +1,24 @@
+"""
+Returns a value in reverse order
+"""
+def reverse_of(value):
+    return value[::-1]
+
+"""
+Returns a set of unique values where identical is
+defined as either naturally equal or equal in reverse
+"""
+def palindromic_unique(values):
+
+    collection = set()
+
+    for value in values:
+        if reverse_of(value) not in collection:
+            collection.add(value)
+
+    return collection
+
+def answer(access_codes):
+    return len(palindromic_unique(access_codes))
+
+print(answer(["x", "y", "xy", "yy", "", "yx"]))
