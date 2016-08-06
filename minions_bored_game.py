61bbdda41d5be483b2710c736f3c2ce7fad3e076
Rudi Theunissen <rudolf.theunissen@gmail.com>
2016-08-07 04:23:24 +1200

    Add some comments to explain bored game

--- minions_bored_game.py
+++ minions_bored_game.py
@@ -44,4 +44,66 @@ n and t will be positive integers, no more than 1000. n will be at least 2.
 """
 
 def answer(t, n):
-    pass
+
+    # Create an empty state accumulator, with an extra space on the left avoid
+    # the need to check for bounds on the left. We can just ignore it.
+    acc = [0] * (n + 1)
+
+    # The initial state is where the token is on square one.
+    acc[1] = 1
+
+    # Update the state for every roll, keeping track of the number of ways we
+    # can get to each square up to that stage.
+    for k in xrange(t):
+
+        # Create an empty state to keep track of this roll's contribution
+        # to the accumulated state.
+        tmp = [0] * (n + 1)
+
+        # The total number of possiblities carries over into the last square.
+        tmp[n] = acc[n]
+
+        # We want to update the number of possible sequences to get to k,
+        # while also taking backtracking into account.
+        for pos in xrange(max(1, n - t + k), n):
+
+            # Update each square's possible movements.
+            for move in (-1, 0, 1):
+                tmp[pos + move] += acc[pos]
+
+        # The temporary state becomes the new accumulated state.
+        acc = tmp
+
+    # The total number of possible sequences will be in the last square of the
+    # accumulated state (to get to that square), so return the requested mod.
+    return acc[n] % 123454321
+
+
+"""
+Recursive solution - far too slow.
+"""
+# LEFT  = -1
+# STAY  =  0
+# RIGHT =  1
+
+# def play(move, distance, distance_to_go, moves_remaining):
+#
+#     moves_remaining -= 1      # Decrease the number of moves to go
+#     distance_to_go  -= move   # Update how far from the target we are
+#     distance        += move   # Update how far we have moved from zero
+
+#     # Can't complete the game or moving off the edge
+#     if (moves_remaining < distance_to_go) or (distance < 0):
+#         return 0
+
+#     # No moves left or have to stay
+#     if (moves_remaining == 0) or (distance_to_go == 0):
+#         return 1
+
+#     return play(RIGHT, distance, distance_to_go , moves_remaining) \
+#          + play(STAY,  distance, distance_to_go , moves_remaining) \
+#          + play(LEFT,  distance, distance_to_go , moves_remaining)
+
+# def answer(moves_remaining, size):
+#     return play(RIGHT, 0, size - 1, moves_remaining) \
+#          + play(STAY,  0, size - 1, moves_remaining)
